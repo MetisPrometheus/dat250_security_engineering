eb11e2225085e00500439c2f11bc219b09642bd8
Kronos <ivar.walskaar@gmail.com>
2019-10-05 14:13:36 +0200

    Added lots of regex and input validators. Also limited maxlength for inputfields

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, FormField, TextAreaField, FileField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FormField, TextAreaField, FileField, TextField
 from wtforms.fields.html5 import DateField
 from wtforms import validators
 
@@ -9,17 +9,26 @@ from wtforms import validators
 # TODO: There was some important security feature that wtforms provides, but I don't remember what; implement it
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', render_kw={'placeholder': 'Username'})
+    username = StringField('Username', render_kw={'placeholder': 'Username', 'maxlength' : 30}) 
     password = PasswordField('Password', render_kw={'placeholder': 'Password'})
     remember_me = BooleanField('Remember me') # TODO: It would be nice to have this feature implemented, probably by using cookies
     submit = SubmitField('Sign In')
 
 class RegisterForm(FlaskForm):
-    first_name = StringField('First Name', render_kw={'placeholder': 'First Name'}, validators=[validators.InputRequired()])
-    last_name = StringField('Last Name', render_kw={'placeholder': 'Last Name'}, validators=[validators.InputRequired()])
-    username = StringField('Username', render_kw={'placeholder': 'Username'}, validators=[validators.InputRequired()])
-    password = PasswordField('Password', render_kw={'placeholder': 'Password'}, validators=[validators.InputRequired()])
-    confirm_password = PasswordField('Confirm Password', render_kw={'placeholder': 'Confirm Password'}, validators=[validators.InputRequired(), validators.Length(min=8, max=50)])
+    first_name = StringField('First Name', render_kw={'placeholder': 'First Name', 'maxlength' : 50}, 
+        validators=[validators.Regexp(r'^[\w]+$', message="Can only contain alphanumeric characters."), validators.InputRequired(), validators.Length(max=50)])
+
+    last_name = StringField('Last Name', render_kw={'placeholder': 'Last Name', 'maxlength' : 80}, 
+        validators=[validators.Regexp(r'^[\w]+$', message="Can only contain alphanumeric characters."), validators.InputRequired(), validators.Length(max=80)])
+
+    username = StringField('Username', render_kw={'placeholder': 'Username', 'maxlength' : 30}, 
+        validators=[validators.Regexp(r'^[\w]+$', message="Username can't contain spaces or special characters"), validators.Length(min=4, max=30)])
+
+    password = PasswordField('Password', render_kw={'placeholder': 'Password'}, 
+        validators=[validators.Regexp(r'^[\S]+$', message="Password can't contain spaces"), validators.InputRequired(), validators.Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', render_kw={'placeholder': 'Confirm Password'}, 
+        validators=[validators.Regexp(r'^[\S]+$', message="Password can't contain spaces"), validators.InputRequired(), validators.Length(min=8)])
+
     submit = SubmitField('Sign Up')
 
 class IndexForm(FlaskForm):
@@ -27,23 +36,37 @@ class IndexForm(FlaskForm):
     register = FormField(RegisterForm)
 
 class PostForm(FlaskForm):
-    content = TextAreaField('New Post', render_kw={'placeholder': 'What are you thinking about?'}, validators=[validators.InputRequired()])
+    content = TextAreaField('New Post', render_kw={'placeholder': 'What are you thinking about?', 'maxlength' : 2000}, 
+        validators=[validators.InputRequired(), validators.Length(max=2000)])
     image = FileField('Image')
     submit = SubmitField('Post')
 
 class CommentsForm(FlaskForm):
-    comment = TextAreaField('New Comment', render_kw={'placeholder': 'What do you have to say?'}, validators=[validators.InputRequired()])
+    comment = TextAreaField('New Comment', render_kw={'placeholder': 'What do you have to say?', 'maxlength' : 2000}, 
+        validators=[validators.InputRequired(), validators.Length(max=2000)])
     submit = SubmitField('Comment')
 
 class FriendsForm(FlaskForm):
-    username = StringField('Friend\'s username', render_kw={'placeholder': 'Username'}, validators=[validators.InputRequired()])
+    username = StringField('Friend\'s username', render_kw={'placeholder': 'Username', 'maxlength' : 30}, 
+        validators=[validators.Regexp(r'^[\w]+$', message="Usernames can't contain spaces or special characters"), 
+        validators.InputRequired(), validators.Length(max=30)])
     submit = SubmitField('Add Friend')
 
 class ProfileForm(FlaskForm):
-    education = StringField('Education', render_kw={'placeholder': 'Highest education'})
-    employment = StringField('Employment', render_kw={'placeholder': 'Current employment'})
-    music = StringField('Favorite song', render_kw={'placeholder': 'Favorite song'})
-    movie = StringField('Favorite movie', render_kw={'placeholder': 'Favorite movie'})
-    nationality = StringField('Nationality', render_kw={'placeholder': 'Your nationality'})
+    education = StringField('Education', render_kw={'placeholder': 'Highest education', 'maxlength' : 80},
+        validators=[validators.InputRequired(), validators.Length(max=80),
+        validators.Regexp(r'^[\w]+$', message="Can only contain alphanumeric characters.")])
+    employment = StringField('Employment', render_kw={'placeholder': 'Current employment', 'maxlength' : 80}, 
+        validators=[validators.InputRequired(), validators.Length(max=80),
+        validators.Regexp(r'^[\w]+$', message="Can only contain alphanumeric characters.")])
+    music = StringField('Favorite song', render_kw={'placeholder': 'Favorite song', 'maxlength' : 80}, 
+        validators=[validators.InputRequired(), validators.Length(max=80),
+        validators.Regexp(r'^[\w]+$', message="Can only contain alphanumeric characters.")])
+    movie = StringField('Favorite movie', render_kw={'placeholder': 'Favorite movie', 'maxlength' : 80}, 
+        validators=[validators.InputRequired(), validators.Length(max=80),
+        validators.Regexp(r'^[\w]+$', message="Can only contain alphanumeric characters.")])
+    nationality = StringField('Nationality', render_kw={'placeholder': 'Your nationality', 'maxlength' : 80},
+        validators=[validators.InputRequired(), validators.Length(max=80),
+        validators.Regexp(r'^[\w]+$', message="Can only contain alphanumeric characters.")])
     birthday = DateField('Birthday')
     submit = SubmitField('Update Profile')
