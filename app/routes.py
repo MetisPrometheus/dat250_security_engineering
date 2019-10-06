4fbd5835cfea257b22d3b943712c179870d885e1
urmanural <46632019+urmanural@users.noreply.github.com>
2019-10-06 15:37:56 +0200

    added generic error edirect to generic error page

--- app/routes.py
+++ app/routes.py
@@ -155,6 +155,10 @@ def add_security_headers(resp):
     resp.headers['X-Content-Type-Options'] = 'nosniff'
     return resp
 
-# @app.errorhandler(CSRFError)
-# def handle_csrf_error(e):
-#     return render_template('csrf_error.html', reason=e.description), 400
+#@app.errorhandler(CSRFError)
+#def handle_csrf_error(e):
+#    return render_template('400.html', reason=e.description), 400
+
+@app.errorhandler(Exception)
+def handle_error(e):
+        return render_template('error.html')
