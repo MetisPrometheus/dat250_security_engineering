376e742e97b7046a9e0295a1df547e3249af04c8
Kronos <ivar.walskaar@gmail.com>
2019-10-07 22:33:47 +0200

    Added two more response headers

--- app/routes.py
+++ app/routes.py
@@ -161,9 +161,11 @@ def profile(username):
 
 @app.after_request
 def add_security_headers(resp):
+    response.headers['Content-Security-Policy'] = "default-src 'self'"
     resp.headers['X-Frame-Options'] = 'SAMEORIGIN'
-    resp.headers['X-XSS-Protection'] = 1
+    response.headers['X-XSS-Protection'] = '1; mode=block'
     resp.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
     return resp
 
 @app.errorhandler(Exception)
